ed97ccd68b54d31036c72b606634153568ec2e5d
Yacine <yyyacine.sellier@gmail.com>
2026-03-25 10:58:42 +0100

    Modification du exo3.py

--- src/main/exo3.py
+++ src/main/exo3.py
@@ -16,16 +16,11 @@ from dataclasses import dataclass
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "src")))
 
 from agents.team1.agent1 import Agent1
-from agents.team2.agent2 import Agent2
-from agents.team3.agent3 import Agent3
-from agents.team4.agent4 import Agent4
-from agents.team5.agent5 import Agent5
-from agents.team6.agent6 import Agent6
-from agents.team7.agent7 import Agent7
+#Suppression des agents 2 à 7
 from pystk2_gymnasium.envs import STKRaceMultiEnv, AgentSpec
 from pystk2_gymnasium.definitions import CameraMode
 
-MAX_TEAMS = 7
+MAX_TEAMS = 1 #Changement pour indiquer uniquement une équipe
 MAX_STEPS = 1000
 NB_RACES = 1
 
@@ -102,12 +97,7 @@ def create_race():
     names = []
 
     agents.append(Agent1(env, path_lookahead=3))
-    agents.append(Agent2(env, path_lookahead=3))
-    agents.append(Agent3(env, path_lookahead=3))
-    agents.append(Agent4(env, path_lookahead=3))
-    agents.append(Agent5(env, path_lookahead=3))
-    agents.append(Agent6(env, path_lookahead=3))
-    agents.append(Agent7(env, path_lookahead=3))
+    #Suppression des agents 2 à 7
     np.random.shuffle(agents)
 
     for i in range(MAX_TEAMS):
