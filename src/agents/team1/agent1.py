225dd1d99cfd86017f4d4c2cf321e0b570b60a3e
Yacine <yyyacine.sellier@gmail.com>
2026-03-25 11:33:38 +0100

    Modification agent1 pour exo3a

--- src/agents/team1/agent1.py
+++ src/agents/team1/agent1.py
@@ -1,43 +1,71 @@
-from pathlib import Path
 import numpy as np
 import random
 from utils.track_utils import compute_curvature, compute_slope
-from omegaconf import OmegaConf
-
 from agents.kart_agent import KartAgent
-from agents.team1.agent_center import AgentCenter
-from agents.team1.agent_speed import AgentSpeed
-from agents.team1.agent_obstacles import AgentObstacles
-from agents.team1.agent_rescue import AgentRescue
-from agents.team1.agent_items import AgentItems
-from agents.team1.agent_drift import AgentDrift
+from .agent1_MidPilot import Agent1Mid
+from .agent1_HalfTurn import Agent1HalfTurn
+from omegaconf import OmegaConf 
+import os
 
 class Agent1(KartAgent):
-    def __init__(self, env, path_lookahead=3):
+    """
+    Agent principal 'Donkey Bombs' (Team 5).
+    Cette classe agit comme un orchestrateur (Wrapper global) qui assemble les différents 
+    """
+    def __init__(self, env, path_lookahead=3, cfg=None):
+        """
+        Initialise l'agent complet en chargeant la configuration YAML et en 
+        emboîtant les différents pilotes les uns dans les autres.
+
+        Args:
+            env (obj): L'environnement de simulation SuperTuxKart.
+            path_lookahead (int): Nombre de points de cheminement à anticiper (défaut: 3).
+        """
         super().__init__(env)
         self.path_lookahead = path_lookahead
-        self.agent_positions = []
-        self.obs = None
+        self.name = "YacineSELLIER"
         self.isEnd = False
-        self.name = "Tasty Crousteam"
 
-        path_conf = Path(__file__).resolve().parent
-        path_conf = str(path_conf) + '/ConfigFileTeam1.yaml'   #Chemin du fichier de configuration
-        self.conf = OmegaConf.load(path_conf)                           #Importation du fichier de configuration
+        # On trouve le chemin de notre fichier actuel
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        
+        # On créer le chemin /src/agent/team7/config.yaml
+        config_path = os.path.join(current_dir, "config.yaml")
 
-        self.agentCenter = AgentCenter(env, self.conf, self.path_lookahead)
-        self.agentSpeed = AgentSpeed(env, self.conf, self.agentCenter, self.path_lookahead)
-        self.agentObstacles = AgentObstacles(env, self.conf, self.agentSpeed, self.path_lookahead)
-        self.agentRescue = AgentRescue(env, self.conf, self.agentObstacles)
-        self.agentItems = AgentItems(env, self.conf, self.agentRescue)
-        self.AgentDrift = AgentDrift(env, self.conf, self.agentItems)
+        # On charge le fichier conf avec ce chemin
+        self.conf = OmegaConf.load(config_path)
+        if cfg is not None:
+            self.conf = cfg
+        
+        # On crée le Pilote qui suit la piste 
+        self.pilot = Agent1Mid(env, self.conf, path_lookahead)
 
-    def reset(self):
-        self.obs, _ = self.env.reset()
-        self.agent_positions = []
+        self.rescue = Agent1HalfTurn(env, self.pilot, self.conf)
 
     def endOfTrack(self):
+        """
+        Indique si le kart a atteint la fin de la piste.
+
+        Returns:
+            bool: True si la fin de la piste est atteinte, False sinon.
+        """
         return self.isEnd
 
+    def reset(self):
+        """Réinitialise la chaîne complète des pilotes (Brain et couches inférieures)."""
+        self.brain.reset()  
+
     def choose_action(self, obs):
-        return self.agentItems.choose_action(obs)
+        """
+        Méthode d'entrée principale du simulateur. 
+        Elle appelle soit le wrapper qui permet d'avancer, soit le wrapper qui permet de reculer.
+
+        Args:
+            obs (dict): Dictionnaire contenant les observations de l'environnement (vitesse, position, etc.).
+
+        Returns:
+            dict: Dictionnaire d'actions (steer, acceleration, brake, drift, nitro, etc.).
+        """
+
+        return self.rescue.choose_action(obs) #On effectue le demi-tour, et une fois effectué, c'est ce wrapper qui appelera notre agentMid
+
